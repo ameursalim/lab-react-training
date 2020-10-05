6a1f84754795cc4d34a3e93341aebac33d547cb3
salim ameur <salim.ameur@gmail.com>
2020-10-05 16:54:04 +0200

    iteration 1

---
+++ src/components/IdCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const IdCard = (props) => {
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+  
+    return (
+
+        <div className="id-card">
+      <img src={props.picture} alt={`Profile of ${props.firstName} ${props.lastName}`}/>
+      <div className="box-card">
+        <p>Last Name: <b>{props.lastName}</b></p>
+        <p>First Name: <b>{props.firstName}</b></p>
+        <p>Gender: <b>{props.gender}</b></p>
+        <p>Height: <b>{props.height}</b></p>
+        <p>Birth: <b>{props.birth.toLocaleDateString(options)}</b></p>
+       
+      </div>
+    </div>
+    )
+  
+};
+
+export default IdCard
